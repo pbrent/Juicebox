38412dc4de21b89288f32f5e48c65c0e82adde5d
pbrent <pbrent99@gmail.com>
2022-07-25 14:44:01 -0500

    starting juicebox pt 2

---
+++ api/posts.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const postsRouter = express.Router();
+const { getAllPosts } = require('../db');
+
+postsRouter.use ((res, req, next) => {
+    console.log("A request is being made to /posts");
+
+    next();
+});
+
+postsRouter.get ('/', async (req, res) => {
+    const posts = await getAllPosts();
+
+    res.send({
+        "posts": []
+    });
+})
+
+module.exports = postsRouter;
